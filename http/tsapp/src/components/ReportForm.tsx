53d72d350253e4781a73323a37e239eb27dbd743
Geunseong Jung <dreamwayjgs@gmail.com>
2019-06-07 18:48:55 +0900

    (feat) ReportForm made

---
+++ http/tsapp/src/components/ReportForm.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../App.css'
+class ReportForm extends Component {
+    render(){
+        return(
+            <div>
+                양식들
+            </div>
+        )
+    }
+}
+
+export default ReportForm;
